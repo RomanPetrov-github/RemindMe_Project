017f01094413d1c776ab837717699b7234597a04
Roman Petrov <dev.petrov.roman@gmail.com>
2017-02-15 06:19:10 +0300

    Created up_header and button notification

--- Reminder/app/src/main/java/com/shockwave/reminder/MainActivity.java
+++ Reminder/app/src/main/java/com/shockwave/reminder/MainActivity.java
@@ -2,23 +2,27 @@ package com.shockwave.reminder;
 
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
 
         initToolbar();
+        InitNavigationView();
     }
 
     private void initToolbar() {
@@ -31,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu_toolbar);
+    }
+
+    private void InitNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
